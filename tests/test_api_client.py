61edee11b9bf991d2458f77da39e39278c3b6ea3
Igor Matchenko <igor.matchenko@nixsolutions.com>
2018-11-15 12:48:25 +0200

    updated tests for new logic

--- tests/test_api_client.py
+++ tests/test_api_client.py
@@ -13,9 +13,9 @@ class TestResponse:
     def raise_for_status(self):
         pass
 
-    def json(self):
+    def json(self, expires_in=60 * 60 * 12):
         return {'access_token': 'test-token',
-                'expires_in': 60 * 60}
+                'expires_in': expires_in}
 
 
 class TestApiClient(unittest.TestCase):
@@ -23,22 +23,23 @@ class TestApiClient(unittest.TestCase):
         self.client = CoreApiClient('https://site.com/', 'test-client-id', 'test-client-secret', 1)
 
     def test_set_token(self):
-        self.client.token = 'test-token'
-        with shelve.open('.cache') as file:
-            self.assertEqual(file['CORE_API_TOKEN'], 'test-token')
+        self.client.token = TestResponse().json()
+        with shelve.open('.cache') as shelve_file:
+            self.assertEqual(shelve_file['API_TOKEN'], 'test-token')
         os.remove('.cache')
 
     def test_get_token(self):
-        with shelve.open('.cache') as file:
-            file['CORE_API_TOKEN'] = 'test-token'
-        self.assertEqual(self.client.token, 'test-token')
+        with shelve.open('.cache') as shelve_file:
+            shelve_file['API_TOKEN'] = 'test-token'
+        self.assertEqual(self.client.token['API_TOKEN'], 'test-token')
         os.remove('.cache')
 
-    @mock.patch.object(CoreApiClient, '_get_token', return_value='test-token')
-    def test_header(self, mock__get_token):
+    @mock.patch.object(CoreApiClient, '_set_token')
+    def test_header(self, mock__set_token):
         """
         Test if header is valid
         """
+        self.client.token = TestResponse().json()
         self.assertEqual(self.client.get_headers(), {'Accept': 'application/json;',
                                                      'Authorization': 'Bearer test-token'})
         self.assertEqual(self.client.get_headers({'key': 0}), {'Accept': 'application/json;',
@@ -52,9 +53,9 @@ class TestApiClient(unittest.TestCase):
         """
         self.assertEqual(self.client.get_api_absolute_url('/poa/'), 'https://site.com/v1/poa/')
 
-    @mock.patch.object(CoreApiClient, '_get_token', return_value=TestResponse())
+    @mock.patch.object(CoreApiClient, '_set_token', return_value=TestResponse())
     @mock.patch.object(CoreApiClient, 'perform_request', return_value=TestResponse())
-    def test_401_error(self, mock_perform_request, mock__get_token):
+    def test_401_error(self, mock_perform_request, mock__set_token):
         """
         Check if you received a 401 error, went to get a token
         """
@@ -62,30 +63,27 @@ class TestApiClient(unittest.TestCase):
         self.assertEqual(mock_perform_request.call_count, 2)
 
     def test_is_token_expired(self):
-        self.client.expires_at = datetime.now() - timedelta(minutes=10)
+        # token is active for 12h
+        self.client.token = TestResponse().json(expires_in=-5 * 60)
         self.assertTrue(self.client.is_token_expired)
-
-        self.client.expires_at = datetime.now()
-        self.assertFalse(self.client.is_token_expired)
         os.remove('.cache')
 
-    def test_expires_at(self):
-        """
-        Check if return date is valid
-        """
-        now = datetime.now()
-        self.client.expires_at = now
-        self.assertEqual(self.client.expires_at,
-                         datetime.strptime(str(now), '%Y-%m-%d %H:%M:%S.%f'))
-
     @mock.patch('requests.post', return_value=TestResponse())
-    def test__get_toke(self, mock_requests_post):
+    def test__set_token(self, mock_requests_post):
         """
         Check if return valid token
         """
-        token = self.client._get_token()
-        self.assertEqual(token, 'test-token')
+        self.client._set_token()
+        self.assertEqual(self.client.token['API_TOKEN'], 'test-token')
         os.remove('.cache')
+
+    def test_working_without_cachse_file(self):
+        """
+        Check if we choose to not use cache file, file is not created
+        """
+        client = CoreApiClient('https://site.com/', 'test-client-id', 'test-client-secret', 1, None)
+        client.token = TestResponse().json()
+        self.assertFalse(os.path.isfile('.cache'))
 
 
 if __name__ == '__main__':
